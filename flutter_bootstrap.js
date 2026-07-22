9a81d3af4ac5d9b5c98f171596222806a55a5a0f
SYA <sya@noreply.invalid>
2026-07-22 23:28:24 +0900

    build(real): 4742dda 시점의 진짜 웹

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2852136757" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "905179320" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
